9bf528a20bc34b27561167a9edbf730ca696726f
EliaAR <alonsoramos.elia@gmail.com>
2022-01-13 19:45:55 +0100

    add endpoint insert songs

---
+++ pages/api/insertSongs.ts
@@ -0,0 +1,26 @@
+import { NextApiHandler } from "next";
+import { pool } from "../../lib/db";
+
+const text =
+  "INSERT INTO songs(id_room, name_song, url_song) VALUES($1, $2, $3) RETURNING *";
+
+const insertSong: NextApiHandler = async (req, res) => {
+  const { id_room, name_song, url_song } = req.body;
+
+  try {
+    if (!id_room || !name_song || !url_song) {
+      return res
+        .status(400)
+        .json({ message: "`name_song` and `url_song` are both required" });
+    }
+
+    const results = await pool.query(text, [id_room, name_song, url_song]);
+
+    return res.json(results);
+  } catch (e: any) {
+    console.error(e);
+    res.status(500).json({ message: e?.message });
+  }
+};
+
+export default insertSong;
