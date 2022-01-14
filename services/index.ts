bcd1472310d3ace420b2794fb653609105ac7e7a
EliaAR <alonsoramos.elia@gmail.com>
2022-01-14 21:46:45 +0100

    create services

---
+++ services/index.ts
@@ -0,0 +1,23 @@
+import slugify from "slugify";
+
+interface createRoomProps {
+  nameRoom: string;
+}
+
+// function getSongs() {
+//   const ENDPOINT = location.origin + "/api/songs?id_room=1";
+//   return fetch(ENDPOINT).then((response) => response.json());
+// }
+
+function createRoom({ nameRoom }: createRoomProps) {
+  const ENDPOINT = location.origin + "/api/rooms/create";
+  return fetch(ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name_room: nameRoom, url_room: slugify(nameRoom) }),
+  }).then((response) => response.json());
+}
+
+export { createRoom };
