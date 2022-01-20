7ed495351b4d24ef1637c781c8f81b85139f939d
EliaAR <alonsoramos.elia@gmail.com>
2022-01-20 11:30:28 +0100

    add some design changes to header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,31 +1,49 @@
+import { useContext } from "react";
+import { useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Switch from "@mui/material/Switch";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { ColorModeContext } from "../../config/theme";
 
 interface HeaderProps {
   title: string;
 }
 
 function Header({ title }: HeaderProps) {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "primary.main" }}>
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography
-            variant="h6"
+            variant="h5"
             color="inherit"
-            component="div"
-            sx={{ flexGrow: 1 }}
+            component="h1"
+            sx={{ flexGrow: 1, pl: 5, fontWeight: "bold" }}
             align="center"
           >
             {title}
           </Typography>
+          <Switch
+            checked={theme.palette.mode === "dark"}
+            onChange={colorMode.toggleColorMode}
+          />
+          {theme.palette.mode === "dark" ? (
+            <Brightness4Icon />
+          ) : (
+            <Brightness7Icon />
+          )}
         </Toolbar>
       </AppBar>
     </Box>
