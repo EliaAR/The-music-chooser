0b37e8fa852cfd1d1c54195900f12f9a497d89a3
EliaAR <alonsoramos.elia@gmail.com>
2023-09-10 20:33:59 +0200

    move youtube-dl to microservice

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,5 +63,14 @@ module.exports = {
         "@typescript-eslint/no-empty-function": "off",
       },
     },
+    {
+      files: ["services/back/youtubedl/fetchYoutubeData.ts"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+      },
+    },
   ],
 };
--- services/back/youtubedl/fetchYoutubeData.ts
+++ services/back/youtubedl/fetchYoutubeData.ts
@@ -1,18 +1,14 @@
-import youtubedl from "youtube-dl-exec";
-
 async function fetchYoutubeData(url: string) {
-  const youtubeOptions = await youtubedl(url, {
-    dumpSingleJson: true,
-    noWarnings: true,
-    noCheckCertificates: true,
-    preferFreeFormats: true,
-    youtubeSkipDashManifest: true,
-  });
+  const ENDPOINT = process.env.FASTAPI_ENDPOINT as string;
+  const encodeURL = encodeURIComponent(url);
+
+  const result = await fetch(ENDPOINT + encodeURL);
+  const youtubeOptions = await result.json();
 
   const name_song = youtubeOptions.title;
   const img = youtubeOptions.thumbnails[0].url;
   const findAudio = youtubeOptions.formats.find(
-    (format) => format.format_id === "249",
+    (format: any) => format.format_id === "249",
   );
   const audio = findAudio?.url || "";
 
