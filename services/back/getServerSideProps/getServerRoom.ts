611e52b0cb395a380cb227861f883156943f3b1e
EliaAR <alonsoramos.elia@gmail.com>
2023-09-11 15:26:51 +0200

    add custom 404, add LoaderComponent, change id routing to ServerSideProps

---
+++ services/back/getServerSideProps/getServerRoom.ts
@@ -0,0 +1,20 @@
+import { NextPageContext } from "next";
+import { getRoom } from "../room/get";
+
+const getServerRoom = async (context: NextPageContext) => {
+  try {
+    const roomDataPN = await getRoom({ id_room: context.query.id as string });
+
+    if ("error" in roomDataPN) throw new Error(roomDataPN.error);
+    return {
+      props: { roomDataPN },
+    };
+  } catch (e) {
+    console.log(e, "error");
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export { getServerRoom };
